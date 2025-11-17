39aedfe19f5616d818f5eb4d6924ffd063617ee9
Tushar Kumar <tkgoyal104@gmail.com>
2025-11-17 17:34:13 +0530

    Advanced Task 3 Updated

--- src/tasks/advanced/bst/TreeNode.java
+++ src/tasks/advanced/bst/TreeNode.java
@@ -1,4 +1,12 @@
 package tasks.advanced.bst;
 
 public class TreeNode {
+    int value;
+    TreeNode left;
+    TreeNode right;
+
+    public TreeNode(int value){
+        this.value = value;
+        left = right = null;
+    }
 }
